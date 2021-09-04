0d3dad19663b61e02eccca99e7a3c3519f07882e
kuerbis-martin <kuerbis.martin.sw@gmail.com>
2021-09-03 18:20:23 -0700

    Add empty Python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+
+import setuptools
+
+BASE_DIR = os.path.dirname(__file__)
+VERSION_FILENAME = os.path.join(BASE_DIR, "opentelemetry_distro_solarwinds",
+                                "version.py")
+PACKAGE_INFO = {}
+with open(VERSION_FILENAME) as f:
+    exec(f.read(), PACKAGE_INFO)
+
+setuptools.setup(version=PACKAGE_INFO["__version__"])
