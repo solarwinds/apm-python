777e67f59efacae65d03255e53a491f845270733
kuerbis-martin <kuerbis.martin.sw@gmail.com>
2021-09-07 15:56:08 -0700

    Store version in setup.cfg

--- setup.py
+++ setup.py
@@ -92,15 +92,7 @@ ext_modules = [
               runtime_library_dirs=['$ORIGIN']),
 ]
 
-BASE_DIR = os.path.dirname(__file__)
-VERSION_FILENAME = os.path.join(BASE_DIR, "opentelemetry_distro_solarwinds",
-                                "version.py")
-PACKAGE_INFO = {}
-with open(VERSION_FILENAME) as f:
-    exec(f.read(), PACKAGE_INFO)
-
 setup(
-    version=PACKAGE_INFO["__version__"],
     cmdclass={
         'build_ext': CustomBuildExt,
     },
