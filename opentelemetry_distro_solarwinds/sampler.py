c380527b37ed7e0165854ceecd88e39572245850
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-03-09 16:49:02 -0800

    Refactor/Break up _SwSampler.should_sample into several helpers

--- opentelemetry_distro_solarwinds/sampler.py
+++ opentelemetry_distro_solarwinds/sampler.py
@@ -3,7 +3,6 @@
 The custom sampler will fetch sampling configurations for the SolarWinds backend.
 """
 
-from inspect import trace
 import logging
 from types import MappingProxyType
 from typing import Optional, Sequence
@@ -18,27 +17,125 @@ from opentelemetry_distro_solarwinds.extension.oboe import Context
 
 logger = logging.getLogger(__name__)
 
-
-def oboe_to_otel_decision(do_metrics, do_sample):
-    """Helper to make OTel decision from oboe outputs"""
-    decision = Decision.DROP
-    if do_metrics:
-        # TODO: need to eck what record only actually means and how metrics work in OTel
-        decision = Decision.RECORD_ONLY
-    if do_sample:
-        decision = Decision.RECORD_AND_SAMPLE
-    return decision
-
 class _SwSampler(Sampler):
     """SolarWinds custom opentelemetry sampler which obeys configuration options provided by the NH/AO Backend."""
     def __init__(
         self,
         is_root_span: bool = False
     ):
+        self._attributes = None
+        self._decision = None
+        self._do_metrics = None
+        self._do_sample = None
         self._is_root_span = is_root_span
+        self._parent_span_context = None
+        self._span_id = None
+        self._trace_state = None
 
     def get_description(self) -> str:
         return "SolarWinds custom opentelemetry sampler"
+
+    def oboe_to_otel_decision(self) -> None:
+        """Helper to make OTel decision from oboe outputs"""
+        decision = Decision.DROP
+        if self._do_metrics:
+            # TODO: need to eck what record only actually means and how metrics work in OTel
+            decision = Decision.RECORD_ONLY
+        if self._do_sample:
+            decision = Decision.RECORD_AND_SAMPLE
+        return decision
+
+    def make_trace_decision(self, trace_id: int) -> None:
+        """Helper to make oboe trace decision"""
+        # TODO: Double-check formats liboboe expects
+        self._do_metrics, self._do_sample, \
+        _, _, _, _, _, _, _, _, _ = Context.getDecisions(
+            f"{trace_id:032X}".lower(),
+            f"{self._span_id:016X}-{self._parent_span_context.trace_flags:02X}".lower()
+        )
+
+    def set_trace_state(self, trace_id: int) -> None:
+        """Helper to set trace_state for sampling result based on parent span.
+        Makes trace decision if needed
+        """
+        # traceparent none (root span) or not valid
+        # or tracestate none or not parsable
+        # or tracestate does not include valid sw
+        if not self._parent_span_context.is_valid \
+            or not self._trace_state \
+            or not self._trace_state.get("sw", None):
+
+            # New sampling decision
+            self.make_trace_decision(trace_id)
+
+            # New tracestate with result of sampling decision
+            self._trace_state = TraceState([(
+                "sw",
+                f"{self._span_id:016X}-{self._do_sample:02X}".lower()
+            )])
+            logger.debug(f"New trace_state: {self._trace_state}")
+
+        # tracestate has valid sw included
+        else:
+            # Continue existing sw sampling decision
+            self._do_sample = self._trace_state.get("sw").split('-')[1]
+            if not self._do_sample in ["00", "01"]:
+                raise Exception(f"Something went wrong checking tracestate: {self._do_sample}")
+
+            # Update trace_state with span_id and sw trace_flags
+            self._trace_state = self._trace_state.update(
+                "sw",
+                f"{self._span_id:016X}-{self._do_sample}".lower()
+            )
+            logger.debug(f"Updated trace_state: {self._trace_state}")
+
+    def set_decision(self) -> None:
+        """Helper to set OTel decision for sampling result"""
+        self._decision = self.oboe_to_otel_decision()
+        logger.debug(f"decision for otel: {self._decision}")
+
+    def set_attributes(self) -> None:
+        """Helper to set attributes based on parent span"""
+        # Set attributes with sw.tracestate_parent_id and sw.w3c.tracestate
+        logger.debug(f"Received attributes: {self._attributes}")
+        if not self._attributes:
+            self._attributes = {
+                "sw.parent_span_id": f"{self._span_id:016X}".lower(),
+                "sw.tracestate_parent_id": f"{self._span_id:016X}".lower(),
+                "sw.w3c.tracestate": self._trace_state.to_header()
+            }
+            logger.debug(f"Set new attributes: {self._attributes}")
+        else:
+            # Copy existing MappingProxyType KV into new_attributes for modification
+            # attributes may have other vendor info etc
+            new_attributes = {}
+            for k,v in self._attributes.items():
+                new_attributes[k] = v
+
+            # Add or Update sw KV in sw.w3c.tracestate
+            if not new_attributes.get("sw.w3c.tracestate", None) or new_attributes.get("sw.w3c.tracestate"):
+                new_attributes["sw.w3c.tracestate"] = self._trace_state.to_header()
+            else:
+                attr_trace_state = TraceState.from_header(new_attributes["sw.w3c.tracestate"])
+                attr_trace_state.update(
+                    "sw",
+                    f"{self._span_id:016X}-{self._do_sample}".lower()
+                )
+                new_attributes["sw.w3c.tracestate"] = attr_trace_state.to_header()
+
+            new_attributes["sw.parent_span_id"] = f"{self._span_id:016X}".lower()
+            # Only set sw.tracestate_parent_id on the entry (root) span for this service
+            # TODO: Or only at root span for the trace?
+            #       Or if sw.tracestate_parent_id is not set?
+            # if self._is_root_span:
+            new_attributes["sw.tracestate_parent_id"] = f"{self._span_id:016X}".lower()
+
+            # Replace
+            self._attributes = new_attributes
+            logger.debug(f"Set updated attributes: {self._attributes}")
+        
+        # attributes must be immutable
+        self._attributes = MappingProxyType(self._attributes)
 
     def should_sample(
         self,
@@ -54,104 +151,24 @@ class _SwSampler(Sampler):
         Use liboboe trace decision for returned OTel SamplingResult
         """
         # TraceContextTextMapPropagator extracts trace_state etc to SpanContext
-        parent_span_context = get_current_span(
+        self._parent_span_context = get_current_span(
             parent_context
         ).get_span_context()
-        
-        do_metrics = None
-        do_sample = None
-        span_id = parent_span_context.span_id
-        trace_state = parent_span_context.trace_state
+        self._span_id = self._parent_span_context.span_id
+        self._trace_state = self._parent_span_context.trace_state
+        self._attributes = attributes
 
-        # traceparent none (root span) or not valid
-        # or
-        # tracestate none or not parsable
-        # or
-        # tracestate does not include valid sw
-        if not parent_span_context.is_valid \
-            or not trace_state \
-            or not trace_state.get("sw", None):
-
-            # New sampling decision
-            # TODO: Double-check formats liboboe expects
-            do_metrics, do_sample, \
-            _, _, _, _, _, _, _, _, _ = Context.getDecisions(
-                f"{trace_id:032X}".lower(),
-                f"{span_id:016X}-{parent_span_context.trace_flags:02X}".lower()
-            )
-
-            # New tracestate with result of sampling decision
-            trace_state = TraceState([(
-                "sw",
-                f"{span_id:016X}-{do_sample:02X}".lower()
-            )])
-            logger.debug(f"New trace_state: {trace_state}")
-
-        # tracestate has valid sw included
-        else:
-            # Continue existing sw sampling decision
-            do_sample = trace_state.get("sw").split('-')[1]
-            if not do_sample in ["00", "01"]:
-                raise Exception(f"Something went wrong checking tracestate: {do_sample}")
-
-            # Update trace_state with span_id and sw trace_flags
-            trace_state = trace_state.update(
-                "sw",
-                f"{span_id:016X}-{do_sample}".lower()
-            )
-            logger.debug(f"Updated trace_state: {trace_state}")
-        
-        decision = oboe_to_otel_decision(do_metrics, do_sample)
-        logger.debug(f"decision for otel: {decision}")
-
-
-        # Set attributes with sw.tracestate_parent_id and sw.w3c.tracestate
-        logger.debug(f"Received attributes: {attributes}")
-        if not attributes:
-            attributes = {
-                "sw.parent_span_id": f"{span_id:016X}".lower(),
-                "sw.tracestate_parent_id": f"{span_id:016X}".lower(),
-                "sw.w3c.tracestate": trace_state.to_header()
-            }
-            logger.debug(f"Set new attributes: {attributes}")
-        else:
-            # Copy existing MappingProxyType KV into new_attributes for modification
-            # attributes may have other vendor info etc
-            new_attributes = {}
-            for k,v in attributes.items():
-                new_attributes[k] = v
-
-            # Add or Update sw KV in sw.w3c.tracestate
-            if not new_attributes.get("sw.w3c.tracestate", None) or new_attributes.get("sw.w3c.tracestate"):
-                new_attributes["sw.w3c.tracestate"] = trace_state.to_header()
-            else:
-                attr_trace_state = TraceState.from_header(new_attributes["sw.w3c.tracestate"])
-                attr_trace_state.update(
-                    "sw",
-                    f"{span_id:016X}-{do_sample}".lower()
-                )
-                new_attributes["sw.w3c.tracestate"] = attr_trace_state.to_header()
-
-            new_attributes["sw.parent_span_id"] = f"{span_id:016X}".lower()
-            # Only set sw.tracestate_parent_id on the entry (root) span for this service
-            # TODO: Or only at root span for the trace?
-            #       Or if sw.tracestate_parent_id is not set?
-            # if self._is_root_span:
-            new_attributes["sw.tracestate_parent_id"] = f"{span_id:016X}".lower()
-
-            # Replace
-            attributes = new_attributes
-            logger.debug(f"Set updated attributes: {attributes}")
-        
-        # attributes must be immutable
-        attributes = MappingProxyType(attributes)
+        # Set args for new SamplingResult
+        self.set_trace_state(trace_id)
+        self.set_decision()
+        self.set_attributes()
 
         # Return result to start_span caller
         # start_span creates a new SpanContext and Span after sampling_result
         return SamplingResult(
-            decision,
-            attributes if decision != Decision.DROP else None,
-            trace_state,
+            self._decision,
+            self._attributes if self._decision != Decision.DROP else None,
+            self._trace_state,
         )
 
 
