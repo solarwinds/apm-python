783cac0f4c807e6529eb67d0fdc93bc31746afa2
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-04-11 15:32:32 -0700

    distro set_global_textmap hardcode instead of env var for OTEL_PROPAGATORS

--- opentelemetry_distro_solarwinds/distro.py
+++ opentelemetry_distro_solarwinds/distro.py
@@ -1,12 +1,19 @@
 """Module to configure OpenTelemetry agent to work with SolarWinds backend"""
 
 from opentelemetry import trace
+from opentelemetry.baggage.propagation import W3CBaggagePropagator
 from opentelemetry.instrumentation.distro import BaseDistro
 from opentelemetry.instrumentation.propagators import set_global_response_propagator
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.composite import CompositePropagator
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace.propagation.tracecontext import (
+    TraceContextTextMapPropagator,
+)
 
 from opentelemetry_distro_solarwinds.exporter import SolarWindsSpanExporter
+from opentelemetry_distro_solarwinds.propagator import SolarWindsPropagator
 from opentelemetry_distro_solarwinds.response_propagator import SolarWindsTraceResponsePropagator
 from opentelemetry_distro_solarwinds.sampler import ParentBasedSwSampler
 
@@ -24,5 +31,13 @@ class SolarWindsDistro(BaseDistro):
         # Automatically configure the SolarWinds Span exporter
         span_exporter = BatchSpanProcessor(SolarWindsSpanExporter())
         trace.get_tracer_provider().add_span_processor(span_exporter)
+        # Configure a CompositePropagator including SolarWinds
+        set_global_textmap(
+            CompositePropagator([
+                TraceContextTextMapPropagator(),
+                W3CBaggagePropagator(),
+                SolarWindsPropagator()
+            ])
+        )
         # Set global HTTP response propagator
         set_global_response_propagator(SolarWindsTraceResponsePropagator())
