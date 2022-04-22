7b75e65f4df0a784060b072472c0f75fa0fa9081
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-04-22 12:15:39 -0700

    Separate concerns: env vars in distro, component init in configurator

---
+++ opentelemetry_distro_solarwinds/configurator.py
@@ -0,0 +1,82 @@
+"""Module to initialize OpenTelemetry SDK components to work with SolarWinds backend"""
+
+import logging
+from os import environ
+from pkg_resources import iter_entry_points
+
+from opentelemetry import trace
+from opentelemetry.environment_variables import (
+    OTEL_PROPAGATORS,
+    OTEL_TRACES_EXPORTER
+)
+from opentelemetry.instrumentation.propagators import set_global_response_propagator
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.composite import CompositePropagator
+from opentelemetry.sdk._configuration import _OTelSDKConfigurator
+from opentelemetry.sdk.environment_variables import OTEL_TRACES_SAMPLER
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry_distro_solarwinds.distro import SolarWindsDistro
+from opentelemetry_distro_solarwinds.response_propagator import SolarWindsTraceResponsePropagator
+
+logger = logging.getLogger(__name__)
+
+class SolarWindsConfigurator(_OTelSDKConfigurator):
+    """OpenTelemetry Configurator for initializing SolarWinds-reporting SDK components"""
+
+    def _configure(self, **kwargs):
+        # If default_traces_sampler is configured then hook up
+        # Else let OTel Python get_from_env_or_default
+        environ_sampler = environ.get(OTEL_TRACES_SAMPLER)
+        if environ_sampler == SolarWindsDistro.default_sw_traces_sampler():
+            try:
+                sampler = next(
+                    iter_entry_points(
+                        "opentelemetry_traces_sampler",
+                        environ_sampler
+                    )).load()()
+            except:
+                logger.exception(
+                    "Failed to load configured sampler `%s`", environ_sampler
+                )
+                raise
+            trace.set_tracer_provider(
+                TracerProvider(sampler=sampler))
+        else:
+            trace.set_tracer_provider()
+
+        environ_exporter = environ.get(OTEL_TRACES_EXPORTER)
+        try:
+            exporter = next(
+                iter_entry_points(
+                    "opentelemetry_traces_exporter",
+                    environ_exporter
+                )).load()()
+        except:
+            logger.exception(
+                "Failed to load configured exporter `%s`", environ_exporter
+            )
+            raise
+        span_exporter = BatchSpanProcessor(exporter)
+        trace.get_tracer_provider().add_span_processor(span_exporter)
+
+        # Init and set CompositePropagator globally, like OTel API
+        environ_propagators = environ.get(OTEL_PROPAGATORS).split(",")
+        propagators = []
+        for propagator in environ_propagators:
+            try:
+                propagators.append(
+                    next(
+                        iter_entry_points("opentelemetry_propagator", propagator)
+                    ).load()()
+                )
+            except Exception:
+                logger.exception(
+                    "Failed to load configured propagator `%s`", propagator
+                )
+                raise
+        set_global_textmap(CompositePropagator(propagators))
+
+        # Set global HTTP response propagator
+        set_global_response_propagator(SolarWindsTraceResponsePropagator())
--- opentelemetry_distro_solarwinds/distro.py
+++ opentelemetry_distro_solarwinds/distro.py
@@ -1,87 +1,52 @@
-"""Module to configure OpenTelemetry agent to work with SolarWinds backend"""
+"""Module to configure OpenTelemetry to work with SolarWinds backend"""
 
 import logging
 from os import environ
-from pkg_resources import iter_entry_points
 
-from opentelemetry import trace
-from opentelemetry.environment_variables import OTEL_PROPAGATORS, OTEL_TRACES_EXPORTER
+from opentelemetry.environment_variables import (
+    OTEL_PROPAGATORS,
+    OTEL_TRACES_EXPORTER
+)
 from opentelemetry.instrumentation.distro import BaseDistro
-from opentelemetry.instrumentation.propagators import set_global_response_propagator
-from opentelemetry.propagate import set_global_textmap
-from opentelemetry.propagators.composite import CompositePropagator
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
-from opentelemetry_distro_solarwinds.response_propagator import SolarWindsTraceResponsePropagator
-from opentelemetry_distro_solarwinds.sampler import ParentBasedSwSampler
+from opentelemetry.sdk.environment_variables import OTEL_TRACES_SAMPLER
 
 logger = logging.getLogger(__name__)
 
 class SolarWindsDistro(BaseDistro):
-    """SolarWinds custom distro for OpenTelemetry agents."""
+    """OpenTelemetry Distro for SolarWinds reporting environment"""
 
-    _DEFAULT_OTEL_EXPORTER = "solarwinds_exporter"
-    _DEFAULT_OTEL_PROPAGATORS = [
+    _DEFAULT_SW_PROPAGATORS = [
         "tracecontext",
         "baggage",
         "solarwinds_propagator",
     ]
+    _DEFAULT_SW_TRACES_EXPORTER = "solarwinds_exporter"
+    _DEFAULT_SW_TRACES_SAMPLER = "solarwinds_sampler"
 
     def _configure(self, **kwargs):
-        # Automatically use custom SolarWinds sampler
-        trace.set_tracer_provider(
-            TracerProvider(sampler=ParentBasedSwSampler()))
-
-        # Customize Exporter else default to SolarWindsSpanExporter
-        environ_exporter = environ.get(
-            OTEL_TRACES_EXPORTER,
-            self._DEFAULT_OTEL_EXPORTER
-        )
-        try:
-            exporter = next(
-                iter_entry_points(
-                    "opentelemetry_traces_exporter",
-                    environ_exporter
-                )).load()()
-        except:
-            logger.exception(
-                "Failed to load configured exporter `%s`", environ_exporter
-            )
-            raise
-
-        span_exporter = BatchSpanProcessor(exporter)
-        trace.get_tracer_provider().add_span_processor(span_exporter)
-
+        environ.setdefault(OTEL_TRACES_SAMPLER, self._DEFAULT_SW_TRACES_SAMPLER)
+        environ.setdefault(OTEL_TRACES_EXPORTER, self._DEFAULT_SW_TRACES_EXPORTER)
+        
         # Configure context propagators to always include
         # tracecontext,baggage,solarwinds -- first and in that order
         # -- plus any others specified by env var
         environ_propagators = environ.get(
             OTEL_PROPAGATORS,
-            ",".join(self._DEFAULT_OTEL_PROPAGATORS)
+            ",".join(self._DEFAULT_SW_PROPAGATORS)
         ).split(",")
-        if environ_propagators != self._DEFAULT_OTEL_PROPAGATORS:
-            for default in self._DEFAULT_OTEL_PROPAGATORS:
+        if environ_propagators != self._DEFAULT_SW_PROPAGATORS:
+            for default in self._DEFAULT_SW_PROPAGATORS:
                 while default in environ_propagators:
                     environ_propagators.remove(default)
-            environ_propagators = self._DEFAULT_OTEL_PROPAGATORS + environ_propagators
+            environ_propagators = self._DEFAULT_SW_PROPAGATORS + environ_propagators
         environ[OTEL_PROPAGATORS] = ",".join(environ_propagators)
 
-        # Init and set CompositePropagator globally, like OTel API
-        propagators = []
-        for propagator in environ_propagators:
-            try:
-                propagators.append(
-                    next(
-                        iter_entry_points("opentelemetry_propagator", propagator)
-                    ).load()()
-                )
-            except Exception:
-                logger.exception(
-                    "Failed to load configured propagator `%s`", propagator
-                )
-                raise
-        set_global_textmap(CompositePropagator(propagators))
-
-        # Set global HTTP response propagator
-        set_global_response_propagator(SolarWindsTraceResponsePropagator())
+        logger.debug("Configured SolarWindsDistro: {}, {}, {}".format(
+            environ.get(OTEL_TRACES_SAMPLER),
+            environ.get(OTEL_TRACES_EXPORTER),
+            environ.get(OTEL_PROPAGATORS)
+        ))
+    
+    @classmethod
+    def default_sw_traces_sampler(cls):
+        return cls._DEFAULT_SW_TRACES_SAMPLER
