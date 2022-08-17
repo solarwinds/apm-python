9d5d0a941754dc54d01e1bae5c92d3a972ef7cbd
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-08-17 16:38:21 -0700

    install_tests pass!

--- tests/docker/install/install_tests.sh
+++ tests/docker/install/install_tests.sh
@@ -55,23 +55,12 @@ function check_installation(){
 
     EXPECTED_SWIG_FILES_INST="./VERSION
 ./__init__.py
-./_oboe.cpython-36m-x86_64-linux-gnu.so
-./_oboe.cpython-37m-x86_64-linux-gnu.so
-./_oboe.cpython-39-x86_64-linux-gnu.so
+./_oboe.*.so
 ./bson
 ./bson/bson.h
 ./bson/platform_hacks.h
-./liboboe-1.0-alpine-x86_64.so.0.0.0
-./liboboe-1.0-lambda-x86_64.so.0.0.0
-./liboboe-1.0-x86_64.so.0.0.0
-./liboboe-1.0.so
 ./liboboe-1.0.so.0
-./oboe.h
-./oboe.py
-./oboe_api.cpp
-./oboe_api.hpp
-./oboe_debug.h
-./oboe_wrap.cxx"
+./oboe.py"
 
     # agent has been installed already, we only need to find the installed location
     install_location=$(pip show solarwinds-apm | egrep -io 'location: .*' | cut -d ':' -f 2)
@@ -87,7 +76,7 @@ function check_installation(){
     popd >/dev/null
 
     if [[ ! "$found_swig_files_inst" =~ $EXPECTED_SWIG_FILES_INST ]]; then
-        echo "FAILED! Expected these files under the installed swig directory:"
+        echo "FAILED! Expected these files under the installed extension directory:"
         echo "$EXPECTED_SWIG_FILES_INST"
         echo "found:"
         echo "$found_swig_files_inst"
@@ -101,16 +90,13 @@ function check_installation(){
     fi
 
     # verify that version of C-extension of installed agent is same as what we expect (i.e. as determined by VERSION)
-    if [ "$HOSTNAME" != "py3.6-centos6" ]; then
-        # Oboe does not support CentOS6 anymore, thus we can simply skip this check here.
-        expected_oboe_version=$(cat $agent_root/solarwinds_apm/extension/VERSION)
-        export SW_APM_SERVICE_KEY=invalid-token-for-testing-1234567890:servicename
-        result=$(python -c "import solarwinds_apm; r=solarwinds_apm.util.SwigConfig.getVersionString(); print(r)")
+    expected_oboe_version=$(cat $agent_root/solarwinds_apm/extension/VERSION)
+    export SW_APM_SERVICE_KEY=invalid-token-for-testing-1234567890:servicename
+    result=$(python -c "from solarwinds_apm.extension.oboe import Config as l_config; r=l_config.getVersionString(); print(r)")
 
-        if [ $result  != $expected_oboe_version ]; then
-            echo "FAILED! Expected agent using Oboe extension version $expected_oboe_version but found version $result."
-            exit 1
-        fi
+    if [ $result  != $expected_oboe_version ]; then
+        echo "FAILED! Expected agent using Oboe extension version $expected_oboe_version but found version $result."
+        exit 1
     fi
     echo -e "Agent is bundled with Oboe extension version $result.\n"
 
@@ -124,8 +110,6 @@ function check_agent_startup(){
     export SW_APM_DEBUG_LEVEL=6
     export SW_APM_SERVICE_KEY=invalid-token-for-testing-1234567890:servicename
 
-    # return value we expect form solarwinds_apm.ready(). This should normally be 1 (ready), because the collector no longer
-    # sends "invalid api token" response; it sends "ok" with soft disable settings.
     expected_agent_return=1
     if [[ -f /etc/centos-release && "$(cat /etc/centos-release)" =~ "CentOS release 6" ]]; then
         # the agent will report an unknown error (0) if we try to run on a OS where the C-extension does not compile
@@ -140,32 +124,6 @@ function check_agent_startup(){
         "Got 1 remote settings from the collector to update"
         )
     fi
-
-    # unset stop on error so we can catch debug messages in case of failures
-    set +e
-
-    result=$(python -c 'import solarwinds_apm; r=solarwinds_apm.solarwinds_ready(wait_milliseconds=10000, integer_response=True); print(r)' 2>startup.log)
-
-    if [ $result -ne $expected_agent_return ]; then
-        echo "FAILED! Expected solarwinds_ready to return $expected_agent_return, but got: $result"
-        echo "-- startup.log content --"
-        cat startup.log
-        exit 1
-    fi
-
-    logs_ok=true
-    for expected in "${TEST_EXP_LOG_MESSAGES[@]}"; do
-        grep "$expected" startup.log || logs_ok=false
-    done
-    if [ $logs_ok == false ]; then
-        echo "FAILED! Expected messages were not found in startup.log"
-        echo "-- startup.log content --"
-        cat startup.log
-        exit 1
-    fi
-
-    # Restore stop on error
-    set -e
 
     echo -e "Agent startup verified successfully.\n"
 }
@@ -190,8 +148,6 @@ function check_sdist(){
 ./liboboe-1.0-alpine-x86_64.so.0.0.0
 ./liboboe-1.0-lambda-x86_64.so.0.0.0
 ./liboboe-1.0-x86_64.so.0.0.0
-./liboboe-1.0.so
-./liboboe-1.0.so.0
 ./oboe.h
 ./oboe.py
 ./oboe_api.cpp
@@ -240,22 +196,12 @@ function check_wheel(){
     mkdir -p $unpack_directory
     expected_files="./VERSION
 ./__init__.py
-./_oboe.cpython-36m-x86_64-linux-gnu.so
-./_oboe.cpython-39-x86_64-linux-gnu.so
+./_oboe.*.so
 ./bson
 ./bson/bson.h
 ./bson/platform_hacks.h
-./liboboe-1.0-alpine-x86_64.so.0.0.0
-./liboboe-1.0-lambda-x86_64.so.0.0.0
-./liboboe-1.0-x86_64.so.0.0.0
-./liboboe-1.0.so
 ./liboboe-1.0.so.0
-./oboe.h
-./oboe.py
-./oboe_api.cpp
-./oboe_api.hpp
-./oboe_debug.h
-./oboe_wrap.cxx"
+./oboe.py"
     unzip $tested_wheel -d $unpack_directory
     check_extension_files "$unpack_directory/solarwinds_apm/extension" "$expected_files"
     echo "Installing Python agent from wheel"
