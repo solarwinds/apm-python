7f660dd186336e87a06382316f5a7064a41bfd6b
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-08-25 11:59:47 -0700

    More local spans for /test trace

--- .github/utils/app.py
+++ .github/utils/app.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask import Flask, request
+from flask import Flask
 from opentelemetry import trace
 
 app = Flask(__name__)
@@ -10,6 +10,11 @@ tracer = trace.get_tracer(__name__)
 def test_trace():
     """Makes request traced by autoinstrumentation
     and a trace started manually with SDK"""
-    with tracer.start_as_current_span("test_trace_manual_span"):
-        requests.get("http://www.solarwinds.com/")
-        return "Done"
+    with tracer.start_as_current_span("test_manual_outer"):
+        current_span = trace.get_current_span()
+        current_span.set_attribute("test.custom_attribute", "outer-foo-bar")
+        with tracer.start_as_current_span("test_manual_inner"):
+            current_span = trace.get_current_span()
+            current_span.set_attribute("test.custom_attribute", "inner-foo-bar")
+            requests.get("http://www.solarwinds.com/")
+            return "Done"
