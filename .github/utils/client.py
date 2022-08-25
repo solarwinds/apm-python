5a7340d77b2471f7829bbcc4c2a8eaabd615523f
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-08-25 13:23:16 -0700

    Adjust client wait times

--- .github/utils/client.py
+++ .github/utils/client.py
@@ -4,10 +4,13 @@ import requests
 
 
 # we need to wait for the Oboe reporter to be ready first, thus add some wait time here
-time.sleep(10)
+time.sleep(30)
 
 # request to instrumented Flask server
 requests.get("http://{}:{}/test/".format(
     os.getenv("FLASK_RUN_HOST"),
     os.getenv("FLASK_RUN_PORT"),
 ))
+
+# we give the reporter more time to finish trace export before exit
+time.sleep(30)
