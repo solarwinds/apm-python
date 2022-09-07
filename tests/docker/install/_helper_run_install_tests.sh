bac6638bc7e550ace9575d81e502592a516e23bf
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-09-06 18:45:50 -0700

    (WIP) Use pyenv for older Ubuntu

--- tests/docker/install/_helper_run_install_tests.sh
+++ tests/docker/install/_helper_run_install_tests.sh
@@ -25,7 +25,7 @@ python_version_no_dot=$(echo "$python_version" | sed 's/\.//')
 pretty_name=$(cat /etc/os-release | grep PRETTY_NAME | sed 's/PRETTY_NAME="//' | sed 's/"//')
 echo "Installing test dependencies for Python $python_version on $pretty_name"
 # setup dependencies quietly
-{
+# {
     if grep Alpine /etc/os-release; then
         # test deps
         apk add bash
@@ -71,23 +71,81 @@ echo "Installing test dependencies for Python $python_version on $pretty_name"
         else
             # Older Ubuntu versions can't use apt/other PPA (e.g. deadsnakes) for Python
             # (libssl incompatibility?)
-            # "Option 2" from https://phoenixnap.com/kb/how-to-install-python-3-ubuntu
+            # Therefore we use pyenv here (further down).
+            # We still use apt-get for other version combinations (e.g. above)
+            # in case pyenv's build dependencies mask SW agent dependencies.
+            # https://realpython.com/intro-to-pyenv/#build-dependencies
+
+            # Install deps
             apt-get upgrade && apt-get update -y
-            apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget make
-            starting_dir=$PWD
-            cd /tmp
-            # Assuming 3.7.9, 3.8.9, 3.9.9 all exist
+            apt-get install -y \
+                git \
+                make \
+                build-essential \
+                zlib1g-dev \
+                libbz2-dev \
+                libreadline-dev \
+                libsqlite3-dev \
+                wget \
+                curl \
+                llvm \
+                libncursesw5-dev \
+                xz-utils \
+                tk-dev \
+                libxml2-dev \
+                libxmlsec1-dev \
+                libffi-dev \
+                liblzma-dev \
+                python-openssl
+            # apt-get install libssl-dev does not install openssl headers
+            # so we do it manually
+            # https://help.dreamhost.com/hc/en-us/articles/360001435926-Installing-OpenSSL-locally-under-your-username
+            first_pwd=$PWD
+            wget --no-check-certificate https://www.openssl.org/source/openssl-1.1.1q.tar.gz.sha256
+            wget --no-check-certificate https://www.openssl.org/source/openssl-1.1.1q.tar.gz
+            if [ ! $(cat ./openssl-1.1.1q.tar.gz.sha256) = $(echo "$(sha256sum ./openssl-1.1.1q.tar.gz)" | cut -d " " -f 1) ]; then
+                echo "ERROR: Could not verify cert of OpenSSL 1.1.1q"
+                exit 1
+            fi
+            tar zxvf ./openssl-1.1.1q.tar.gz
+            cd ./openssl-1.1.1q
+            ./config --prefix=/usr/lib/openssl --openssldir=/usr/lib/openssl no-ssl2
+            make
+            make test
+            make install
+            cd $first_pwd
+            echo 'export PATH=/usr/lib/openssl/bin:$PATH' >> ~/.bash_profile
+            echo 'export LD_LIBRARY_PATH=/usr/lib/openssl/lib' >> ~/.bash_profile
+            echo 'export LC_ALL="C.UTF-8"' >> ~/.bash_profile
+            echo 'export LDFLAGS="-L /usr/lib/openssl/lib -Wl,-rpath,/usr/lib/openssl/lib"' >> ~/.bash_profile
+            echo 'export CPPFLAGS="-I/usr/lib/ssl/include"' >> ~/.bash_profile
+            . ~/.bash_profile
+
+            # Install pyenv
+            # https://github.com/pyenv/pyenv-installer
+            curl https://pyenv.run | bash
+            echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bash_profile
+            echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+            echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
+            . ~/.bash_profile
+            # pyenv git compatibility issue on older versions
+            # https://github.com/pyenv/pyenv-update/issues/13
+            # https://github.com/pyenv/pyenv-update/blob/master/bin/pyenv-update
+            sed -i 's/verify_repo "$1" &&/#verify_repo "$1" &&/' /root/.pyenv/plugins/pyenv-update/bin/pyenv-update
+            sed -i "s/git pull --tags --no-rebase --ff/git pull --tags/" /root/.pyenv/plugins/pyenv-update/bin/pyenv-update
+            pyenv update
+
+            # Then, install python
+            # Assumes 3.7.9, 3.8.9, 3.9.9 all exist
             # https://www.python.org/ftp/python/
-            wget "https://www.python.org/ftp/python/$python_version.9/Python-$python_version.9.tgz"
-            tar -xf "Python-$python_version.9.tgz"
-            cd "Python-$python_version.9"
-            ./configure --enable-optimizations
-            make altinstall  # "This can take up to 30 minutes to complete"
-            sudo make install
-            cd $starting_dir
-            apt-get install -y gcc unzip
-            update-alternatives --install /usr/bin/python python /usr/local/bin/python3 1
-            update-alternatives --install /usr/bin/pip pip "/usr/local/bin/pip$python_version" 1
+            # CPPFLAGS="-I/usr/lib/ssl/include" LDFLAGS="-L/usr/lib/ssl/lib" pyenv install -v "$python_version.9"
+            # echo "openssl version -d" | CPPFLAGS="-I$1/include" LDFLAGS="-L$1/lib" pyenv install -v "$python_version.9"
+            pyenv install -v "$python_version.9"
+            pyenv global "$python_version.9"
+
+            # ???
+            # update-alternatives --install /usr/bin/python python /usr/bin/python3 1
+            # update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
         fi
     
     elif grep "Amazon Linux" /etc/os-release; then
@@ -102,7 +160,7 @@ echo "Installing test dependencies for Python $python_version on $pretty_name"
             findutils
         alternatives --set python "/usr/bin/python$python_version"
     fi
-} >/dev/null
+# } >/dev/null
 
 # Click requires unicode locale
 # https://click.palletsprojects.com/en/8.1.x/unicode-support/
