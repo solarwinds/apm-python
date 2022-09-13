ce232ddfc4b2efcbb00017cc70054a1f6b0b90a8
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-09-13 09:42:42 -0700

    Debug GH workflow

--- tests/docker/install/install_tests.sh
+++ tests/docker/install/install_tests.sh
@@ -334,6 +334,10 @@ function run_instrumented_server_and_client(){
         export SW_APM_TRUSTEDPATH="$PWD/ao_issuer_ca_public_cert.crt"
     fi
     echo "Testing trace export from Flask to $4..."
+
+    # Temp debug
+    opentelemetry-instrument flask run
+
     nohup opentelemetry-instrument flask run > log.txt 2>&1 &
     python client.py
 }
