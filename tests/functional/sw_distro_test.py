b73ac4902d2ded4e9baa88944ca98fb25d5e9be7
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-10-25 10:41:23 -0700

    Add SolarWindsDistroTestBase and refactor

---
+++ tests/functional/sw_distro_test.py
@@ -0,0 +1,88 @@
+"""
+Test base class for SolarWinds APM custom distro.
+Initializes custom otel components with SolarWindsConfigurator.
+Extends OTel TestBase for InMemorySpanExporter.
+"""
+import os
+from pkg_resources import iter_entry_points
+import re
+import requests
+import time
+
+from opentelemetry import trace as trace_api
+from opentelemetry.propagate import get_global_textmap
+from opentelemetry.sdk.trace import export
+from opentelemetry.test.globals_test import reset_trace_globals
+from opentelemetry.test.test_base import TestBase
+
+from solarwinds_apm.apm_config import SolarWindsApmConfig
+from solarwinds_apm.configurator import SolarWindsConfigurator
+from solarwinds_apm.distro import SolarWindsDistro
+from solarwinds_apm.propagator import SolarWindsPropagator
+from solarwinds_apm.sampler import ParentBasedSwSampler
+
+class SolarWindsDistroTestBase(TestBase):
+
+    SW_SETTINGS_KEYS = [
+        "BucketCapacity",
+        "BucketRate",
+        "SampleRate",
+        "SampleSource"
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        # Based on auto_instrumentation run() and sitecustomize.py
+        # Load OTel env vars entry points
+        argument_otel_environment_variable = {}
+        for entry_point in iter_entry_points(
+            "opentelemetry_environment_variables"
+        ):
+            environment_variable_module = entry_point.load()
+            for attribute in dir(environment_variable_module):
+                if attribute.startswith("OTEL_"):
+                    argument = re.sub(r"OTEL_(PYTHON_)?", "", attribute).lower()
+                    argument_otel_environment_variable[argument] = attribute
+
+        # Set APM service key
+        os.environ["SW_APM_SERVICE_KEY"] = "foo:bar"
+
+        # Load Distro
+        SolarWindsDistro().configure()
+        assert os.environ["OTEL_PROPAGATORS"] == "tracecontext,baggage,solarwinds_propagator"
+
+        # Load Configurator to Configure SW custom SDK components
+        # except use TestBase InMemorySpanExporter
+        apm_config = SolarWindsApmConfig()
+        configurator = SolarWindsConfigurator()
+        configurator._initialize_solarwinds_reporter(apm_config)
+        configurator._configure_propagator()
+        configurator._configure_response_propagator()
+        # This is done because set_tracer_provider cannot override the
+        # current tracer provider.
+        reset_trace_globals()
+        configurator._configure_sampler(apm_config)
+        sampler = trace_api.get_tracer_provider().sampler
+        # Set InMemorySpanExporter for testing
+        cls.tracer_provider, cls.memory_exporter = cls.create_tracer_provider(sampler=sampler)
+        span_processor = export.SimpleSpanProcessor(cls.memory_exporter)
+        cls.tracer_provider.add_span_processor(span_processor)
+        trace_api.set_tracer_provider(cls.tracer_provider)
+        cls.tracer = cls.tracer_provider.get_tracer(__name__)
+
+        # Make sure SW SDK components were set
+        propagators = get_global_textmap()._propagators
+        assert len(propagators) == 3
+        assert isinstance(propagators[2], SolarWindsPropagator)
+        assert isinstance(trace_api.get_tracer_provider().sampler, ParentBasedSwSampler)
+
+        cls.composite_propagator = get_global_textmap()
+        cls.tc_propagator = cls.composite_propagator._propagators[0]
+        cls.sw_propagator = cls.composite_propagator._propagators[2]
+    
+    @classmethod
+    def wakeup_request(cls):
+        """Send wake-up request and wait for oboe_init to finish"""
+        with cls.tracer.start_as_current_span("wakeup_span"):
+            r = requests.get(f"http://solarwinds.com")
+            time.sleep(2)
--- tests/functional/test_span_attributes.py
+++ tests/functional/test_span_attributes.py
@@ -1,34 +1,20 @@
 """
 Test span attributes calculation by SW sampler given different OTel
 contexts for root/child spans, extracted headers.
-
-Uses OTel TestBase to check spans with InMemorySpanExporter.
 """
 import hashlib
 import hmac
 import logging
 import os
-from pkg_resources import iter_entry_points
-import re
-import requests
 import sys
 import time
 
 from unittest import mock
 from unittest.mock import patch
 
-from opentelemetry import trace as trace_api
-from opentelemetry.propagate import get_global_textmap
-from opentelemetry.sdk.trace import export
-from opentelemetry.test.globals_test import reset_trace_globals
-from opentelemetry.test.test_base import TestBase
 from opentelemetry.trace.span import TraceState
 
-from solarwinds_apm.apm_config import SolarWindsApmConfig
-from solarwinds_apm.configurator import SolarWindsConfigurator
-from solarwinds_apm.distro import SolarWindsDistro
-from solarwinds_apm.propagator import SolarWindsPropagator
-from solarwinds_apm.sampler import ParentBasedSwSampler
+from .sw_distro_test import SolarWindsDistroTestBase
 
 
 # Logging
@@ -42,71 +28,13 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-class TestSpanAttributes(TestBase):
-
-    SW_SETTINGS_KEYS = [
-        "BucketCapacity",
-        "BucketRate",
-        "SampleRate",
-        "SampleSource"
-    ]
+class TestSpanAttributes(SolarWindsDistroTestBase):
 
     @classmethod
     def setUpClass(cls):
-        # Based on auto_instrumentation run() and sitecustomize.py
-        # Load OTel env vars entry points
-        argument_otel_environment_variable = {}
-        for entry_point in iter_entry_points(
-            "opentelemetry_environment_variables"
-        ):
-            environment_variable_module = entry_point.load()
-            for attribute in dir(environment_variable_module):
-                if attribute.startswith("OTEL_"):
-                    argument = re.sub(r"OTEL_(PYTHON_)?", "", attribute).lower()
-                    argument_otel_environment_variable[argument] = attribute
+        super().setUpClass()
+        cls.wakeup_request()
 
-        # Set APM service key
-        os.environ["SW_APM_SERVICE_KEY"] = "foo:bar"
-
-        # Load Distro
-        SolarWindsDistro().configure()
-        assert os.environ["OTEL_PROPAGATORS"] == "tracecontext,baggage,solarwinds_propagator"
-
-        # Load Configurator to Configure SW custom SDK components
-        # except use TestBase InMemorySpanExporter
-        apm_config = SolarWindsApmConfig()
-        configurator = SolarWindsConfigurator()
-        configurator._initialize_solarwinds_reporter(apm_config)
-        configurator._configure_propagator()
-        configurator._configure_response_propagator()
-        # This is done because set_tracer_provider cannot override the
-        # current tracer provider.
-        reset_trace_globals()
-        configurator._configure_sampler(apm_config)
-        sampler = trace_api.get_tracer_provider().sampler
-        # Set InMemorySpanExporter for testing
-        cls.tracer_provider, cls.memory_exporter = cls.create_tracer_provider(sampler=sampler)
-        span_processor = export.SimpleSpanProcessor(cls.memory_exporter)
-        cls.tracer_provider.add_span_processor(span_processor)
-        trace_api.set_tracer_provider(cls.tracer_provider)
-        cls.tracer = cls.tracer_provider.get_tracer(__name__)
-
-        # Make sure SW SDK components were set
-        propagators = get_global_textmap()._propagators
-        assert len(propagators) == 3
-        assert isinstance(propagators[2], SolarWindsPropagator)
-        assert isinstance(trace_api.get_tracer_provider().sampler, ParentBasedSwSampler)
-
-        cls.composite_propagator = get_global_textmap()
-        cls.tc_propagator = cls.composite_propagator._propagators[0]
-        cls.sw_propagator = cls.composite_propagator._propagators[2]
-
-        # Wake-up request and wait for oboe_init
-        with cls.tracer.start_as_current_span("wakeup_span"):
-            r = requests.get(f"http://solarwinds.com")
-            logger.debug("Wake-up request with headers: {}".format(r.headers))
-            time.sleep(2)
-        
     def test_root_span_attrs(self):
         """Test attribute setting for a root span with no parent"""
         # liboboe mocked to guarantee return of "do_sample" and "start
