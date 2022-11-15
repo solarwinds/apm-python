dbe9b829979bc74707e4983951def32c0a4ba7bd
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-11-14 16:21:52 -0800

    TestBaseSw class and separate scenario classes

---
+++ tests/integration/test_scenario_1.py
@@ -0,0 +1,118 @@
+import re
+import json
+
+from opentelemetry import trace as trace_api
+from unittest import mock
+
+from .test_base_sw_headers_attrs import TestBaseSwHeadersAndAttributes
+
+class TestScenario1(TestBaseSwHeadersAndAttributes):
+    """
+    Test class for starting a new tracing decision with no input headers.
+    """
+
+    def test_scenario_1_sampled(self):
+        """
+        Scenario #1:
+        1. Decision to sample is made at root/service entry span (mocked).
+        2. Some traceparent and tracestate are injected into service's outgoing request (done by OTel TraceContextTextMapPropagator).
+        3. Sampling-related attributes are set for the root/service entry span.
+        4. The span_id of the outgoing request span matches the span_id portion in the tracestate header.
+        """
+        # Use in-process test app client and mock to propagate context
+        # and create in-memory trace
+        resp = None
+        # liboboe mocked to guarantee return of "do_sample" and "start
+        # decision" rate/capacity values in order to trace and set attrs
+        mock_decision = mock.Mock(
+            return_value=(1, 1, 3, 4, 5.0, 6.0, 1, 0, "ok", "ok", 0)
+        )
+        with mock.patch(
+            target="solarwinds_apm.extension.oboe.Context.getDecisions",
+            new=mock_decision,
+        ):
+            # Request to instrumented app, no traceparent/tracestate
+            resp = self.client.get("/test_trace/")
+        resp_json = json.loads(resp.data)
+
+        # Verify trace context injected into test app's outgoing postman-echo call
+        # (added to Flask app's response data) includes:
+        #    - traceparent with a trace_id, span_id, and trace_flags for do_sample
+        #    - tracestate with same span_id and trace_flags for do_sample
+        assert "traceparent" in resp_json
+        _TRACEPARENT_HEADER_FORMAT = (
+            "^[ \t]*([0-9a-f]{2})-([0-9a-f]{32})-([0-9a-f]{16})-([0-9a-f]{2})"
+            + "(-.*)?[ \t]*$"
+        )
+        _TRACEPARENT_HEADER_FORMAT_RE = re.compile(_TRACEPARENT_HEADER_FORMAT)
+        traceparent_re_result = re.search(
+            _TRACEPARENT_HEADER_FORMAT_RE,
+            resp_json["traceparent"],
+        )
+        new_trace_id = traceparent_re_result.group(2)
+        assert new_trace_id
+        new_span_id = traceparent_re_result.group(3)
+        assert new_span_id
+        new_trace_flags = traceparent_re_result.group(4)
+        assert new_trace_flags == "01"
+
+        assert "tracestate" in resp_json
+        assert new_span_id in resp_json["tracestate"]
+        # In this test we know there is only `sw` in tracestate
+        # e.g. sw=e000baa4e000baa4-01
+        _TRACESTATE_HEADER_FORMAT = (
+            "^[ \t]*sw=([0-9a-f]{16})-([0-9a-f]{2})"
+            + "(-.*)?[ \t]*$"
+        )
+        _TRACESTATE_HEADER_FORMAT_RE = re.compile(_TRACESTATE_HEADER_FORMAT)
+        tracestate_re_result = re.search(
+            _TRACESTATE_HEADER_FORMAT_RE,
+            resp_json["tracestate"],
+        )
+        new_tracestate_flags = tracestate_re_result.group(2)
+        assert new_tracestate_flags == "01"
+
+        # Verify x-trace response header has same trace_id
+        # though it will have different span ID because of Flask
+        # app's outgoing request
+        assert "x-trace" in resp.headers
+        assert new_trace_id in resp.headers["x-trace"]
+
+        # Verify spans exported: service entry (root) + outgoing request
+        spans = self.memory_exporter.get_finished_spans()
+        assert len(spans) == 2
+        span_server = spans[1]
+        span_client = spans[0]
+        assert span_server.name == "/test_trace/"
+        assert span_server.kind == trace_api.SpanKind.SERVER
+        assert span_client.name == "HTTP GET"
+        assert span_client.kind == trace_api.SpanKind.CLIENT
+
+        # Check root span attributes
+        #   :present:
+        #     service entry internal KVs, which are on all entry spans
+        #   :absent:
+        #     sw.tracestate_parent_id, because cannot be set without attributes at decision
+        #     SWKeys, because no xtraceoptions in otel context
+        assert all(attr_key in span_server.attributes for attr_key in self.SW_SETTINGS_KEYS)
+        assert span_server.attributes["BucketCapacity"] == "6.0"
+        assert span_server.attributes["BucketRate"] == "5.0"
+        assert span_server.attributes["SampleRate"] == 3
+        assert span_server.attributes["SampleSource"] == 4
+        assert not "sw.tracestate_parent_id" in span_server.attributes
+        assert not "SWKeys" in span_server.attributes
+
+        # Check outgoing request span attributes
+        #   :absent:
+        #     service entry internal KVs, which are only on entry spans
+        #     sw.tracestate_parent_id, because cannot be set without attributes at decision
+        #     SWKeys, because no xtraceoptions in otel context
+        assert not any(attr_key in span_client.attributes for attr_key in self.SW_SETTINGS_KEYS)
+        assert not "sw.tracestate_parent_id" in span_client.attributes
+        assert not "SWKeys" in span_client.attributes
+
+        # Check span_id of the outgoing request span (client span) matches
+        # the span_id portion in the outgoing tracestate header, which
+        # is stored in the test app's response body (new_span_id).
+        # Note: context.span_id needs a 16-byte hex conversion first.
+        assert "{:016x}".format(span_client.context.span_id) == new_span_id
