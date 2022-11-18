1aa006773cf6551a61604ff5d44564c4460a5bde
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2022-11-17 17:29:43 -0800

    Refactor subprocess calls

--- scripts/lint_and_format.py
+++ scripts/lint_and_format.py
@@ -13,44 +13,37 @@ def parse_args(args=None):
     return parser.parse_args(args)
 
 
+def run_subprocess(args, allowexitcodes):
+    """Helper to run process and sys exit if error"""
+    result = subprocess.run(args)
+    if result is not None and result.returncode not in allowexitcodes:
+        print(
+            "'{}' failed with code {}".format(args[0], result.returncode),
+            file=sys.stderr,
+        )
+        sys.exit(result.returncode)
+
+
 def lint_and_format(args):
     black_args = ("black", "--config", "pyproject.toml", "solarwinds_apm")
     if args.check_only:
         black_args += ("--diff", "--check")
-    black_result = subprocess.run(black_args)
-    if black_result is not None and black_result.returncode not in args.allowexitcodes:
-        print(
-            "'{}' failed with code {}".format("black", black_result.returncode),
-            file=sys.stderr,
-        )
-        sys.exit(black_result.returncode)
+    run_subprocess(black_args, args.allowexitcodes)
 
     isort_args = ("isort", "--settings-path", ".isort.cfg", "solarwinds_apm")
     if args.check_only:
         isort_args += ("--diff", "--check")
-    isort_result = subprocess.run(isort_args)
-    if isort_result is not None and isort_result.returncode not in args.allowexitcodes:
-        print(
-            "'{}' failed with code {}".format("isort", isort_result.returncode),
-            file=sys.stderr,
-        )
-        sys.exit(isort_result.returncode)
+    run_subprocess(isort_args, args.allowexitcodes)
 
-    flake_result = subprocess.run(("flake8", "--config", ".flake8", "solarwinds_apm"))
-    if flake_result is not None and flake_result.returncode not in args.allowexitcodes:
-        print(
-            "'{}' failed with code {}".format("flake8", flake_result.returncode),
-            file=sys.stderr,
-        )
-        sys.exit(flake_result.returncode)
+    run_subprocess(
+        ("flake8", "--config", ".flake8", "solarwinds_apm"),
+        args.allowexitcodes,
+    )
 
-    pylint_result = subprocess.run(("pylint", "--ignore", "solarwinds_apm/extension", "solarwinds_apm"))
-    if pylint_result is not None and pylint_result.returncode not in args.allowexitcodes:
-        print(
-            "'{}' failed with code {}".format("pylint", pylint_result.returncode),
-            file=sys.stderr,
-        )
-        sys.exit(pylint_result.returncode)
+    run_subprocess(
+        ("pylint", "--ignore", "solarwinds_apm/extension", "solarwinds_apm"),
+        args.allowexitcodes,
+    )
 
     print("Done.")
 
