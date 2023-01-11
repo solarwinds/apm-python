1e231002ea4d0695949856bb827f6fb060fe4566
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-01-10 16:10:54 -0800

    Add SW 2023 headers to src files

--- .github/scripts/is_publishable.sh
+++ .github/scripts/is_publishable.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
 # User input version for release (e.g. 1.0.0)
 input_version="$1"
 # Version in solarwinds_apm
