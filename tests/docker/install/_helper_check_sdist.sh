3eb55b1844fae06057f5d554294939015af831b4
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-02-03 13:48:51 -0800

    Fix aarch64 check_sdist

--- tests/docker/install/_helper_check_sdist.sh
+++ tests/docker/install/_helper_check_sdist.sh
@@ -125,9 +125,9 @@ function check_sdist(){
 ./bson
 ./bson/bson.h
 ./bson/platform_hacks.h
-./liboboe-1.0-aarch64.so.0.0.0
-./liboboe-1.0-alpine-aarch64.so.0.0.0
-./liboboe-1.0-lambda-aarch64.so.0.0.0
+./liboboe-1.0-aarch64.so
+./liboboe-1.0-alpine-aarch64.so
+./liboboe-1.0-lambda-aarch64.so
 ./oboe.h
 ./oboe.py
 ./oboe_api.cpp
