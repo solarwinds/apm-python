d68f7e186aa7906df3d1ac394a5890f6e6749c28
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-02-14 11:00:52 -0800

    Add set_transaction_name placeholder

--- solarwinds_apm/api/__init__.py
+++ solarwinds_apm/api/__init__.py
@@ -4,20 +4,36 @@
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-"""
-Example usage:
-
-from solarwinds_apm.api import solarwinds_ready
-solarwinds_ready(wait_milliseconds=10000, integer_response=True)
-"""
-
 import logging
 from typing import Any
 
+from opentelemetry.trace import get_current_span, get_tracer_provider
+
 from solarwinds_apm.apm_oboe_codes import OboeReadyCode
 from solarwinds_apm.extension.oboe import Context
+from solarwinds_apm.inbound_metrics_processor import SolarWindsInboundMetricsSpanProcessor
 
 logger = logging.getLogger(__name__)
+
+
+def set_transaction_name(name: str) -> None:
+    # Assumes TracerProvider's active span processor is SynchronousMultiSpanProcessor
+    # or ConcurrentMultiSpanProcessor
+    span_processors = get_tracer_provider()._active_span_processor._span_processors
+    inbound_processor = None
+    for spr in span_processors:
+        if type(spr) == SolarWindsInboundMetricsSpanProcessor:
+            inbound_processor = spr
+
+    if not inbound_processor:
+        logger.error("Could not find configured InboundMetricsSpanProcessor.")
+        return
+
+    current_span = get_current_span()
+    trace_id = current_span.get_span_context().trace_id
+    logger.debug("TODO: Implement me!")
+    # inbound_processor._apm_txname_customizer[trace_id] = name
+    # logger.debug("Cached custom transaction name as %s", name)
 
 
 def solarwinds_ready(
@@ -40,8 +56,8 @@ def solarwinds_ready(
     else:bool True for ready, False not ready,
 
     :Example:
-
-     if not solarwinds_ready(10000):
+     from solarwinds_apm.api import solarwinds_ready
+     if not solarwinds_ready(wait_milliseconds=10000, integer_response=True):
         Logger.info("SolarWinds not ready after 10 seconds, no metrics will be sent")
     """
     rc = Context.isReady(wait_milliseconds)
