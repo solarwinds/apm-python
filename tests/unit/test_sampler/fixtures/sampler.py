4489a344bcd52c642684124fa2b0ca89fec08ddd
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-04-21 16:12:18 -0700

    Fix unit tests

--- tests/unit/test_sampler/fixtures/sampler.py
+++ tests/unit/test_sampler/fixtures/sampler.py
@@ -2,17 +2,22 @@ import pytest
 
 from solarwinds_apm.sampler import _SwSampler
 
+def side_effect_fn(param):
+    if param == "tracing_mode":
+        return -1
+    elif param == "transaction_filters":
+        return []
+
 @pytest.fixture(name="sw_sampler")
 def fixture_swsampler(mocker):
     mock_apm_config = mocker.Mock()
     mock_get = mocker.Mock(
-        return_value=1  # enabled
+        side_effect=side_effect_fn
     )
     mock_apm_config.configure_mock(
         **{
             "agent_enabled": True,
             "get": mock_get,
-            "tracing_mode": None,  # mapped to -1
         }
     )
     return _SwSampler(mock_apm_config)
