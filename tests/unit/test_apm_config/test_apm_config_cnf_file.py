553187b6c727ce9cc024d35d6bc3e53e70203a62
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-04-26 09:56:41 -0700

    Add fixture_cnf_file and test

--- tests/unit/test_apm_config/test_apm_config_cnf_file.py
+++ tests/unit/test_apm_config/test_apm_config_cnf_file.py
@@ -10,6 +10,7 @@ from solarwinds_apm import apm_config
 
 # pylint: disable=unused-import
 from .fixtures.cnf_file import (
+    fixture_cnf_file,
     fixture_cnf_file_invalid_json,
 )
 # pylint: disable=unused-import
@@ -60,8 +61,22 @@ class TestSolarWindsApmConfigCnfFile:
         # cnf_dict is none
         assert resulting_config.get_cnf_dict() is None
 
-    def test_get_cnf_dict(self):
-        pass
+    # pylint:disable=unused-argument
+    def test_get_cnf_dict(
+        self,
+        mocker,
+        fixture_cnf_file,
+    ):
+        mocker.patch.dict(os.environ, {
+            "SW_APM_SERVICE_KEY": "valid:key-service-name",
+            "SW_APM_CONFIG_FILE": "nothing-is-here",
+        })
+        # use key from env var, agent enabled, nothing has errored
+        resulting_config = apm_config.SolarWindsApmConfig()
+        assert resulting_config._calculate_agent_enabled()
+        assert resulting_config.service_name == "key-service-name"
+        # cnf_dict is dict with kv from fixture
+        assert resulting_config.get_cnf_dict() == {"foo": "bar"}
 
     def test_update_with_cnf_file_prependdomain_invalid(self):
         pass
