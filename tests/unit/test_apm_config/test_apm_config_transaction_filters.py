befa6bc4c643240aa5dd5ee8de6add9bc51e2722
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-04-26 17:22:25 -0700

    (WIP) Add TestSolarWindsApmConfigTxnFilters

---
+++ tests/unit/test_apm_config/test_apm_config_transaction_filters.py
@@ -0,0 +1,35 @@
+# © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+from solarwinds_apm import apm_config
+
+class TestSolarWindsApmConfigTxnFilters:
+    def test_update_transaction_filters_none(self):
+        pass
+
+    def test_update_transaction_filters_not_list(self):
+        pass
+
+    def test_update_transaction_filters_missing_tracing(self):
+        pass
+
+    def test_update_transaction_filters_invalid_tracing(self):
+        pass
+
+    def test_update_transaction_filters_missing_regex(self):
+        pass
+
+    def test_update_transaction_filters_invalid_type_regex(self):
+        pass
+
+    def test_update_transaction_filters_empty_regex(self):
+        pass
+
+    def test_update_transaction_filters_invalid_compile_regex(self):
+        pass
+
+    def test_update_transaction_filters_multiple_regex_use_first(self):
+        pass
