2322d6edb27479010f9f34e85456d6f53caf76af
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-06-20 17:16:45 -0700

    Add sqlchemy dep and engine check for sqlcommenter enabling

--- solarwinds_apm/distro.py
+++ solarwinds_apm/distro.py
@@ -6,9 +6,15 @@
 
 """Module to configure OpenTelemetry to work with SolarWinds backend"""
 
+import logging
 from os import environ
 
 from pkg_resources import EntryPoint
+from sqlalchemy import (
+    create_engine,
+    select,
+    String,
+)
 
 from opentelemetry.environment_variables import (
     OTEL_PROPAGATORS,
@@ -24,6 +30,9 @@ from solarwinds_apm.apm_constants import (
     INTL_SWO_DEFAULT_PROPAGATORS,
     INTL_SWO_DEFAULT_TRACES_EXPORTER,
 )
+
+
+logger = logging.getLogger(__name__)
 
 
 class SolarWindsDistro(BaseDistro):
@@ -52,8 +61,33 @@ class SolarWindsDistro(BaseDistro):
         This is a method override to pass additional arguments to each
         entry point.
         """
-        # Enable sqlcommenter feature, if implemented
-        kwargs["enable_commenter"] = True
-        kwargs["is_sql_commentor_enabled"] = True
+        if self.enable_commenter(entry_point, **kwargs):
+            kwargs["enable_commenter"] = True
+            kwargs["is_sql_commentor_enabled"] = True
+            if entry_point.name == "sqlalchemy":
+                engine_url = environ.get("SW_APM_SQLALCHEMY_ENGINE_URL", "")
+                engine = create_engine(engine_url)
+                kwargs["engine"] = engine
+        
+        # debug
+        logger.warning("entry_point.name: %s", entry_point.name)
+        logger.warning("load_instrumentor kwargs: %s", kwargs)
+
         instrumentor: BaseInstrumentor = entry_point.load()
         instrumentor().instrument(**kwargs)
+
+    def enable_commenter(
+        self, entry_point: EntryPoint, **kwargs
+    ) -> bool:
+        """Enable sqlcommenter feature, if implemented"""
+        enable_commenter = environ.get("OTEL_SQLCOMMENTER_ENABLED", "")
+        if enable_commenter.lower() == "true":
+            if entry_point.name == "sqlalchemy":
+                engine_url = environ.get("SW_APM_SQLALCHEMY_ENGINE_URL")
+                # We don't check if engine_url is actually correct
+                if not engine_url:
+                    logger.warning("OTEL_SQLCOMMENTER_ENABLED but SW_APM_SQLALCHEMY_ENGINE_URL not provided. Not enabling sqlcommenter for SQLAlchemy.")
+                    return False
+            else:
+                return True
+        return False
