22f4adfae2c3113b45b1e377fe60c1483a66cc22
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-12-20 14:37:07 -0600

    Add comment

--- solarwinds_apm/configurator.py
+++ solarwinds_apm/configurator.py
@@ -97,6 +97,7 @@ class SolarWindsConfigurator(_OTelSDKConfigurator):
         apm_config = SolarWindsApmConfig()
         # TODO add args
         # https://swicloud.atlassian.net/browse/NH-68264
+        # See also SolarWindsApmConfig._get_extension_components
         oboe_api = apm_config.oboe_api()
 
         # TODO Add experimental trace flag, clean up
