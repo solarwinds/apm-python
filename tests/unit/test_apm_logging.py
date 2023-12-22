d5b417a834a99a1a542e66b175245fccdeac6eb6
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2023-12-22 16:08:47 -0600

    Update apm_logging tests

--- tests/unit/test_apm_logging.py
+++ tests/unit/test_apm_logging.py
@@ -6,13 +6,51 @@
 
 from solarwinds_apm import apm_logging
 
-class TestApmLoggingLevel:
+class TestApmLoggingType:
+    def test_default_level(self):
+        assert apm_logging.ApmLoggingType.default_level() == 0
+    
+    def test_disabled_level(self):
+        assert apm_logging.ApmLoggingType.disabled_level() == 4
 
     def test_is_valid_level_ok(self):
-        assert apm_logging.ApmLoggingLevel.is_valid_level("6")
+        assert apm_logging.ApmLoggingType.is_valid_level(0)
+        assert apm_logging.ApmLoggingType.is_valid_level(1)
+        assert apm_logging.ApmLoggingType.is_valid_level(2)
+        assert apm_logging.ApmLoggingType.is_valid_level(3)
+        assert apm_logging.ApmLoggingType.is_valid_level(4)
+
+    def test_is_valid_level_not_int(self):
+        assert not apm_logging.ApmLoggingType.is_valid_level("abc")
+
+    def test_is_valid_level_int_out_of_range(self):
+        assert not apm_logging.ApmLoggingType.is_valid_level(-9999)
+        assert not apm_logging.ApmLoggingType.is_valid_level(-1)
+        assert not apm_logging.ApmLoggingType.is_valid_level(5)
+        assert not apm_logging.ApmLoggingType.is_valid_level(9999)
+
+class TestApmLoggingLevel:
+    def test_default_level(self):
+        assert apm_logging.ApmLoggingLevel.default_level() == 2
+
+    def test_critical_level(self):
+        assert apm_logging.ApmLoggingLevel.critical_level() == 0
+
+    def test_is_valid_level_ok(self):
+        assert apm_logging.ApmLoggingLevel.is_valid_level(-1)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(0)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(1)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(2)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(3)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(4)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(5)
+        assert apm_logging.ApmLoggingLevel.is_valid_level(6)
 
     def test_is_valid_level_not_int(self):
         assert not apm_logging.ApmLoggingLevel.is_valid_level("abc")
 
     def test_is_valid_level_int_out_of_range(self):
+        assert not apm_logging.ApmLoggingLevel.is_valid_level(-9999)
+        assert not apm_logging.ApmLoggingLevel.is_valid_level(-2)
+        assert not apm_logging.ApmLoggingLevel.is_valid_level(7)
         assert not apm_logging.ApmLoggingLevel.is_valid_level(9999)
