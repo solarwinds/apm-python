4db9a068a3ecc5b100027e3774a4d4a8a23b8474
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-01-29 14:38:02 -0800

    update log level

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ class CustomBuildExt(build_ext):
 
 
 if not (python_version_supported() and os_supported()):
-    logger.warn(
+    logger.warning(
         "[SETUP] This package supports only Python 3.7 and above on Linux x86_64 or aarch64. "
         "Other platform or python versions may not work as expected.")
 
