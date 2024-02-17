3f1a4adffc66011675b959ff8c46fac4d196446d
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-02-16 16:55:54 -0800

    Remove ForceFlushSpanProcessor

--- solarwinds_apm/trace/__init__.py
+++ solarwinds_apm/trace/__init__.py
@@ -1,4 +1,3 @@
-from .forceflush_processor import ForceFlushSpanProcessor
 from .inbound_metrics_processor import SolarWindsInboundMetricsSpanProcessor
 from .otlp_metrics_processor import SolarWindsOTLPMetricsSpanProcessor
 from .serviceentry_processor import ServiceEntryIdSpanProcessor
@@ -6,7 +5,6 @@ from .txnname_calculator_processor import TxnNameCalculatorProcessor
 from .txnname_cleanup_processor import TxnNameCleanupProcessor
 
 __all__ = [
-    "ForceFlushSpanProcessor",
     "ServiceEntryIdSpanProcessor",
     "SolarWindsInboundMetricsSpanProcessor",
     "SolarWindsOTLPMetricsSpanProcessor",
