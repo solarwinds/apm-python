566eb9fd1718eaeefd5ddbcec7fe0e1ddf809fdd
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-03-12 14:58:41 -0700

    Rm unused import

--- tests/unit/test_configurator/test_configurator_create_init_event.py
+++ tests/unit/test_configurator/test_configurator_create_init_event.py
@@ -5,7 +5,6 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 from solarwinds_apm import configurator
-from solarwinds_apm.extension.oboe import Event
 
 from .fixtures.trace import get_trace_mocks
 
