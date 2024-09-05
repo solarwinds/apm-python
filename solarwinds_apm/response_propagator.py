ef3cf114f07c93a3d9bbc0b0b674d011d764ea88
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-09-05 13:13:07 -0700

    lint for py312

--- solarwinds_apm/response_propagator.py
+++ solarwinds_apm/response_propagator.py
@@ -4,8 +4,10 @@
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
+# TODO: Remove when Python < 3.10 support dropped
+from __future__ import annotations
+
 import logging
-import typing
 
 from opentelemetry import trace
 from opentelemetry.context.context import Context
@@ -37,7 +39,7 @@ class SolarWindsTraceResponsePropagator(ResponsePropagator):
     def inject(
         self,
         carrier: textmap.CarrierT,
-        context: typing.Optional[Context] = None,
+        context: Context | None = None,
         setter: textmap.Setter = textmap.default_setter,
     ) -> None:
         """Injects x-trace and options-response into the HTTP response carrier."""
