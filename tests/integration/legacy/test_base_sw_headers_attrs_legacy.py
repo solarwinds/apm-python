17433e81a733442a7405e89a55ab9fc29e612352
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2025-02-06 13:29:36 -0800

    Clean up imports

--- tests/integration/legacy/test_base_sw_headers_attrs_legacy.py
+++ tests/integration/legacy/test_base_sw_headers_attrs_legacy.py
@@ -4,32 +4,6 @@
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-import os
-import re
-
-import flask
-import requests
-from werkzeug.test import Client
-from werkzeug.wrappers import Response
-
-from opentelemetry import trace as trace_api
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.propagate import get_global_textmap
-from opentelemetry.sdk.trace import TracerProvider, export
-from opentelemetry.sdk.trace.export.in_memory_span_exporter import (
-    InMemorySpanExporter,
-)
-from opentelemetry.test.globals_test import reset_trace_globals
-from opentelemetry.util._importlib_metadata import entry_points
-
-from solarwinds_apm.apm_config import SolarWindsApmConfig
-from solarwinds_apm.configurator import SolarWindsConfigurator
-from solarwinds_apm.distro import SolarWindsDistro
-from solarwinds_apm.extension.oboe import OboeAPI
-from solarwinds_apm.propagator import SolarWindsPropagator
-from solarwinds_apm.sampler import ParentBasedSwSampler
-
 from ..test_base_sw_headers_attrs import TestBaseSwHeadersAndAttributes
 
 
