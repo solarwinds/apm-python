efc2dea242329258d1391283fc2e04ed42f0c23e
Jerry Leung <tingfung.leung@solarwinds.com>
2025-02-20 14:21:28 -0800

    strongly typed

--- solarwinds_apm/oboe/dice.py
+++ solarwinds_apm/oboe/dice.py
@@ -1,8 +1,9 @@
-import random
+from random import random
+from typing import Optional
 
 
 class _Dice:
-    def __init__(self, scale, rate=0):
+    def __init__(self, scale : int, rate : int = 0):
         self._scale = scale
         self._rate = rate
 
@@ -11,13 +12,13 @@ class _Dice:
         return self._rate
 
     @rate.setter
-    def rate(self, new_rate):
+    def rate(self, new_rate : int):
         self._rate = max([0, min([self._scale, new_rate])])
 
-    def update(self, new_scale, new_rate=None):
+    def update(self, new_scale : int, new_rate : Optional[int] = None):
         self._scale = new_scale
         if new_rate is not None:
             self.rate = new_rate
 
     def roll(self):
-        return random.random() * self._scale < self.rate
+        return random() * self._scale < self.rate
