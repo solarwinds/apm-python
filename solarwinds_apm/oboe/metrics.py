e50e03969efe404cd663277344be8a4ce7120f70
Jerry Leung <tingfung.leung@solarwinds.com>
2025-02-24 15:35:16 -0800

    Updated

--- solarwinds_apm/oboe/metrics.py
+++ solarwinds_apm/oboe/metrics.py
@@ -3,12 +3,12 @@ from opentelemetry.metrics import get_meter_provider
 class Counters:
     def __init__(self):
         self._meter = get_meter_provider().get_meter("sw.apm.sampling.metrics")
-        self._request_count = self._meter.create_counter(name="trace.service.request_count", value_type=int)
-        self._sample_count = self._meter.create_counter(name="trace.service.samplecount", value_type=int)
-        self._trace_count = self._meter.create_counter(name="trace.service.tracecount", value_type=int)
-        self._through_trace_count = self._meter.create_counter(name="trace.service.through_trace_count", value_type=int)
-        self._triggered_trace_count = self._meter.create_counter(name="trace.service.triggered_trace_count", value_type=int)
-        self._token_bucket_exhaustion_count = self._meter.create_counter(name="trace.service.tokenbucket_exhaustion_count", value_type=int)
+        self._request_count = self._meter.create_counter(name="trace.service.request_count")
+        self._sample_count = self._meter.create_counter(name="trace.service.samplecount")
+        self._trace_count = self._meter.create_counter(name="trace.service.tracecount")
+        self._through_trace_count = self._meter.create_counter(name="trace.service.through_trace_count")
+        self._triggered_trace_count = self._meter.create_counter(name="trace.service.triggered_trace_count")
+        self._token_bucket_exhaustion_count = self._meter.create_counter(name="trace.service.tokenbucket_exhaustion_count")
 
     @property
     def request_count(self):
--- solarwinds_apm/oboe/settings.py
+++ solarwinds_apm/oboe/settings.py
@@ -29,8 +29,24 @@ class BucketType(Enum):
 
 class BucketSettings:
     def __init__(self, capacity: int, rate: int):
-        self.capacity = capacity
-        self.rate = rate
+        self._capacity = capacity
+        self._rate = rate
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, new_capacity):
+        self._capacity = new_capacity
+
+    @property
+    def rate(self):
+        return self._rate
+
+    @rate.setter
+    def rate(self, new_rate):
+        self._rate = new_rate
 
 
 class Settings:
@@ -38,29 +54,103 @@ class Settings:
                  timestamp: int, ttl: int,
                  buckets: Optional[Dict[BucketType, BucketSettings]] = None,
                  signature_key: Optional[str] = None):
-        self.sample_rate = sample_rate
-        self.sample_source = sample_source
-        self.flags = flags
-        self.buckets = buckets if buckets is not None else {}
-        self.signature_key = signature_key
-        self.timestamp = timestamp
-        self.ttl = ttl
+        self._sample_rate = sample_rate
+        self._sample_source = sample_source
+        self._flags = flags
+        self._buckets = buckets if buckets is not None else {}
+        self._signature_key = signature_key
+        self._timestamp = timestamp
+        self._ttl = ttl
+
+    @property
+    def sample_rate(self):
+        return self._sample_rate
+
+    @sample_rate.setter
+    def sample_rate(self, new_sample_rate):
+        self._sample_rate = new_sample_rate
+
+    @property
+    def sample_source(self):
+        return self._sample_source
+
+    @sample_source.setter
+    def sample_source(self, new_sample_source):
+        self._sample_source = new_sample_source
+
+    @property
+    def flags(self):
+        return self._flags
+
+    @flags.setter
+    def flags(self, new_flags):
+        self._flags = new_flags
+
+    @property
+    def buckets(self):
+        return self._buckets
+
+    @buckets.setter
+    def buckets(self, new_buckets):
+        self._buckets = new_buckets
+
+    @property
+    def signature_key(self):
+        return self._signature_key
+
+    @signature_key.setter
+    def signature_key(self, new_signature_key):
+        self._signature_key = new_signature_key
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @timestamp.setter
+    def timestamp(self, new_timestamp):
+        self._timestamp = new_timestamp
+
+    @property
+    def ttl(self):
+        return self._ttl
+
+    @ttl.setter
+    def ttl(self, new_ttl):
+        self._ttl = new_ttl
 
     def __eq__(self, other):
         if not isinstance(other, Settings):
             return NotImplemented
-        return self.sample_rate == other.sample_rate and self.sample_source == other.sample_source and self.flags == other.flags and self.buckets == other.buckets and self.signature_key == other.signature_key and self.timestamp == other.timestamp and self.ttl == other.ttl
+        return self._sample_rate == other._sample_rate and self._sample_source == other._sample_source and self._flags == other._flags and self._buckets == other._buckets and self._signature_key == other._signature_key and self._timestamp == other._timestamp and self._ttl == other._ttl
 
+    def __str__(self):
+        return f"Settings(sample_rate={self._sample_rate}, sample_source={self._sample_source}, flags={self._flags}, buckets={self._buckets}, timestamp={self._timestamp}, ttl={self._ttl})"
 
 class LocalSettings:
     def __init__(self, trigger_mode: bool, tracing_mode: Optional[TracingMode] = None):
-        self.tracing_mode = tracing_mode
-        self.trigger_mode = trigger_mode
+        self._tracing_mode = tracing_mode
+        self._trigger_mode = trigger_mode
+
+    @property
+    def tracing_mode(self):
+        return self._tracing_mode
+
+    @tracing_mode.setter
+    def tracing_mode(self, new_tracing_mode):
+        self._tracing_mode = new_tracing_mode
+
+    @property
+    def trigger_mode(self):
+        return self._trigger_mode
+
+    @trigger_mode.setter
+    def trigger_mode(self, new_trigger_mode):
+        self._trigger_mode = new_trigger_mode
 
     def __eq__(self, other):
         if not isinstance(other, LocalSettings):
             return NotImplemented
-        return self.tracing_mode == other.tracing_mode and self.trigger_mode == other.trigger_mode
+        return self._tracing_mode == other._tracing_mode and self._trigger_mode == other._trigger_mode
 
 
 def merge(remote: Settings, local: LocalSettings) -> Settings:
