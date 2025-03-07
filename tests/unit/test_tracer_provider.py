03e8310730352d6a6bf6101577c9bb11f0a872fa
Jerry Leung <tingfung.leung@solarwinds.com>
2025-03-06 17:01:15 -0800

    Added test

---
+++ tests/unit/test_tracer_provider.py
@@ -0,0 +1,17 @@
+# © 2025 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+from unittest.mock import MagicMock
+
+from solarwinds_apm.oboe.http_sampler import HttpSampler
+from solarwinds_apm.tracer_provider import SolarwindsTracerProvider
+
+
+def test_shutdown_calls_sampler_shutdown():
+    sampler = MagicMock(spec=HttpSampler)
+    provider = SolarwindsTracerProvider(sampler=sampler)
+    provider.shutdown()
+    provider.sampler.shutdown.assert_called_once()
