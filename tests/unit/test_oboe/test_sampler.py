eacebcc899b337ec3b573aea4f75c3ba0ade28cf
Jerry Leung <tingfung.leung@solarwinds.com>
2025-03-07 19:26:03 -0800

    fixed tox

--- tests/unit/test_oboe/test_sampler.py
+++ tests/unit/test_oboe/test_sampler.py
@@ -3,10 +3,11 @@
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+from __future__ import annotations
 
 import logging
 import time
-from typing import Optional, Any
+from typing import Any
 
 from opentelemetry import trace
 from opentelemetry.sdk.metrics import MeterProvider, AlwaysOnExemplarFilter
@@ -36,7 +37,7 @@ class TestSampler(Sampler):
         raise Exception("Test sampler")
 
 
-def options(tracing: Optional[bool], trigger_trace: bool,
+def options(tracing: bool | None, trigger_trace: bool,
             transaction_settings: list[TransactionSetting]) -> Configuration:
     return Configuration(
         tracing_mode=tracing,
@@ -54,7 +55,7 @@ def options(tracing: Optional[bool], trigger_trace: bool,
     )
 
 
-def settings(enabled: bool, signature_key: Optional[str]):
+def settings(enabled: bool, signature_key: str | None):
     return {
         "value": 1_000_000,
         "flags": "SAMPLE_START,SAMPLE_THROUGH_ALWAYS,TRIGGER_TRACE" if enabled else "",
