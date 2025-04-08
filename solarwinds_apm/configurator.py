3df4b56a01fcdc3232f3c80584d3e5627a4cc313
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2025-04-08 14:05:19 -0700

    Comment

--- solarwinds_apm/configurator.py
+++ solarwinds_apm/configurator.py
@@ -4,7 +4,7 @@
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-"""Module to initialize OpenTelemetry SDK components and liboboe to work with SolarWinds backend"""
+"""Module to initialize OpenTelemetry SDK components for SolarWinds backend"""
 
 import importlib
 import logging
