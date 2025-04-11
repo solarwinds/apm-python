c0659b93c38e6dd2343f248796d50b75467f1750
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2025-04-11 15:58:28 -0700

    Test common util function

--- tests/unit/test_configurator/test_configurator_traces_init.py
+++ tests/unit/test_configurator/test_configurator_traces_init.py
@@ -15,19 +15,24 @@ from solarwinds_apm import configurator
 from solarwinds_apm.exporter import SolarWindsSpanExporter
 
 class TestConfiguratorTracingInit:
-    def test_custom_init_tracing_none(
+    def setup_each_test(
         self,
         mocker,
-        mock_apmconfig_enabled,
-        mock_bsprocessor,
-        mock_ssprocessor,
+        mock_apmconfig,
+        exporters=None,
+        otlp_protocol=None,
     ):
+        env_vars = {}
+        if otlp_protocol:
+            env_vars["OTEL_EXPORTER_OTLP_PROTOCOL"] = otlp_protocol
+        mocker.patch.dict(os.environ, env_vars, clear=True)
+
         mocker.patch(
             "solarwinds_apm.configurator.SolarWindsApmConfig",
-            return_value=mock_apmconfig_enabled,
+            return_value=mock_apmconfig,
         )
 
-        # Mock Otel
+        # Mock Otel components
         mock_tracerprovider = mocker.Mock()
         mock_tracerprovider_instance = mocker.Mock()
         mock_tracerprovider.return_value = mock_tracerprovider_instance
@@ -41,20 +46,56 @@ class TestConfiguratorTracingInit:
         mock_apm_sampler = mocker.Mock()
         mock_resource = mocker.Mock()
 
+        # Mock span exporters
+        mock_sw_exporter = mocker.patch(
+            "solarwinds_apm.exporter.SolarWindsSpanExporter",
+        )
+        mock_http_exporter = mocker.patch(
+            "solarwinds_apm.configurator.HttpSpanExporter",
+        )
+        mock_grpc_exporter = mocker.patch(
+            "solarwinds_apm.configurator.GrpcSpanExporter",
+        )
+
+        return {
+            "mock_tracerprovider": mock_tracerprovider,
+            "mock_tracerprovider_instance": mock_tracerprovider_instance,
+            "mock_set_tracer_provider": mock_set_tracer_provider,
+            "mock_apm_sampler": mock_apm_sampler,
+            "mock_resource": mock_resource,
+            "mock_sw_exporter": mock_sw_exporter,
+            "mock_http_exporter": mock_http_exporter,
+            "mock_grpc_exporter": mock_grpc_exporter,
+        }
+
+    def test_custom_init_tracing_none(
+        self,
+        mocker,
+        mock_apmconfig_enabled,
+        mock_bsprocessor,
+        mock_ssprocessor,
+    ):
+        mocks = self.setup_each_test(
+            mocker,
+            mock_apmconfig_enabled,
+        )
+
+        # Test
         test_configurator = configurator.SolarWindsConfigurator()
         test_configurator._custom_init_tracing(
             exporters={},
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_tracerprovider.assert_called_once_with(
+
+        mocks["mock_tracerprovider"].assert_called_once_with(
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_set_tracer_provider.assert_called_once()
-        mock_tracerprovider_instance.add_span_processor.assert_not_called()
+        mocks["mock_set_tracer_provider"].assert_called_once()
+        mocks["mock_tracerprovider_instance"].add_span_processor.assert_not_called()
         mock_bsprocessor.assert_not_called()
         mock_ssprocessor.assert_not_called()
 
@@ -65,24 +106,10 @@ class TestConfiguratorTracingInit:
         mock_bsprocessor,
         mock_ssprocessor,
     ):
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarWindsApmConfig",
-            return_value=mock_apmconfig_enabled,
+        mocks = self.setup_each_test(
+            mocker,
+            mock_apmconfig_enabled,
         )
-
-        # Mock Otel
-        mock_tracerprovider = mocker.Mock()
-        mock_tracerprovider_instance = mocker.Mock()
-        mock_tracerprovider.return_value = mock_tracerprovider_instance
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarwindsTracerProvider",
-            new=mock_tracerprovider,
-        )
-        mock_set_tracer_provider = mocker.patch(
-            "solarwinds_apm.configurator.set_tracer_provider",
-        )
-        mock_apm_sampler = mocker.Mock()
-        mock_resource = mocker.Mock()
 
         # Mock span exporter class
         class MockExporter():
@@ -97,16 +124,16 @@ class TestConfiguratorTracingInit:
         test_configurator._custom_init_tracing(
             exporters={"valid_exporter": MockExporter},
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_apm_sampler"],
         )
-        mock_tracerprovider.assert_called_once_with(
+        mocks["mock_tracerprovider"].assert_called_once_with(
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_apm_sampler"],
         )
-        mock_set_tracer_provider.assert_called_once()
-        mock_tracerprovider_instance.add_span_processor.assert_called_once_with(
+        mocks["mock_set_tracer_provider"].assert_called_once()
+        mocks["mock_tracerprovider_instance"].add_span_processor.assert_called_once_with(
             mock_bsprocessor.return_value,
         )
         mock_exporter_spy.assert_called_once()
@@ -120,24 +147,10 @@ class TestConfiguratorTracingInit:
         mock_bsprocessor,
         mock_ssprocessor,
     ):
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarWindsApmConfig",
-            return_value=mock_apmconfig_enabled_is_lambda,
+        mocks = self.setup_each_test(
+            mocker,
+            mock_apmconfig_enabled_is_lambda,
         )
-
-        # Mock Otel
-        mock_tracerprovider = mocker.Mock()
-        mock_tracerprovider_instance = mocker.Mock()
-        mock_tracerprovider.return_value = mock_tracerprovider_instance
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarwindsTracerProvider",
-            new=mock_tracerprovider,
-        )
-        mock_set_tracer_provider = mocker.patch(
-            "solarwinds_apm.configurator.set_tracer_provider",
-        )
-        mock_apm_sampler = mocker.Mock()
-        mock_resource = mocker.Mock()
 
         # Mock span exporter class
         class MockExporter():
@@ -152,21 +165,21 @@ class TestConfiguratorTracingInit:
         test_configurator._custom_init_tracing(
             exporters={"valid_exporter": MockExporter},
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_tracerprovider.assert_called_once_with(
+        mocks["mock_tracerprovider"].assert_called_once_with(
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_set_tracer_provider.assert_called_once()
-        mock_tracerprovider_instance.add_span_processor.assert_called_once_with(
+        mocks["mock_set_tracer_provider"].assert_called_once()
+        mocks["mock_tracerprovider_instance"].add_span_processor.assert_called_once_with(
             mock_ssprocessor.return_value,
         )
         mock_exporter_spy.assert_called_once()
-        mock_bsprocessor.assert_not_called()
         mock_ssprocessor.assert_called_once()
+        mock_bsprocessor.assert_not_called()
 
     def test_custom_init_tracing_legacy_traces_exporter_protocol_none(
         self,
@@ -175,32 +188,10 @@ class TestConfiguratorTracingInit:
         mock_bsprocessor,
         mock_ssprocessor,
     ):
-        mocker.patch.dict(os.environ, {}, clear=True)
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarWindsApmConfig",
-            return_value=mock_apmconfig_enabled,
-        )
-
-        # Mock Otel
-        mock_tracerprovider = mocker.Mock()
-        mock_tracerprovider_instance = mocker.Mock()
-        mock_tracerprovider.return_value = mock_tracerprovider_instance
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarwindsTracerProvider",
-            new=mock_tracerprovider,
-        )
-        mock_set_tracer_provider = mocker.patch(
-            "solarwinds_apm.configurator.set_tracer_provider",
-        )
-        mock_apm_sampler = mocker.Mock()
-        mock_resource = mocker.Mock()
-
-        # Mock exporters
-        mock_sw_exporter = mocker.patch(
-            "solarwinds_apm.exporter.SolarWindsSpanExporter",
-        )
-        mock_http_exporter = mocker.patch(            
-            "solarwinds_apm.configurator.HttpSpanExporter",
+        mocks = self.setup_each_test(
+            mocker,
+            mock_apmconfig_enabled,
+            exporters={"valid_exporter": SolarWindsSpanExporter},
         )
 
         # Test!
@@ -208,21 +199,21 @@ class TestConfiguratorTracingInit:
         test_configurator._custom_init_tracing(
             exporters={"valid_exporter": SolarWindsSpanExporter},
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_tracerprovider.assert_called_once_with(
+        mocks["mock_tracerprovider"].assert_called_once_with(
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_set_tracer_provider.assert_called_once()
-        mock_tracerprovider_instance.add_span_processor.assert_called_once_with(
+        mocks["mock_set_tracer_provider"].assert_called_once()
+        mocks["mock_tracerprovider_instance"].add_span_processor.assert_called_once_with(
             mock_bsprocessor.return_value,
         )
-        mock_http_exporter.assert_called_once()
-        mock_sw_exporter.assert_not_called()
-        mock_bsprocessor.assert_called_once_with(mock_http_exporter.return_value)
+        mocks["mock_http_exporter"].assert_called_once()
+        mocks["mock_sw_exporter"].assert_not_called()
+        mock_bsprocessor.assert_called_once_with(mocks["mock_http_exporter"].return_value)
         mock_ssprocessor.assert_not_called()
 
     def test_custom_init_tracing_legacy_traces_exporter_protocol_http(
@@ -232,38 +223,11 @@ class TestConfiguratorTracingInit:
         mock_bsprocessor,
         mock_ssprocessor,
     ):
-        mocker.patch.dict(
-            os.environ,
-            {
-                "OTEL_EXPORTER_OTLP_PROTOCOL": "http/protobuf",
-            },
-            clear=True
-        )
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarWindsApmConfig",
-            return_value=mock_apmconfig_enabled,
-        )
-
-        # Mock Otel
-        mock_tracerprovider = mocker.Mock()
-        mock_tracerprovider_instance = mocker.Mock()
-        mock_tracerprovider.return_value = mock_tracerprovider_instance
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarwindsTracerProvider",
-            new=mock_tracerprovider,
-        )
-        mock_set_tracer_provider = mocker.patch(
-            "solarwinds_apm.configurator.set_tracer_provider",
-        )
-        mock_apm_sampler = mocker.Mock()
-        mock_resource = mocker.Mock()
-
-        # Mock exporters
-        mock_sw_exporter = mocker.patch(
-            "solarwinds_apm.exporter.SolarWindsSpanExporter",
-        )
-        mock_http_exporter = mocker.patch(            
-            "solarwinds_apm.configurator.HttpSpanExporter",
+        mocks = self.setup_each_test(
+            mocker,
+            mock_apmconfig_enabled,
+            exporters={"valid_exporter": SolarWindsSpanExporter},
+            otlp_protocol="http/protobuf",
         )
 
         # Test!
@@ -271,21 +235,21 @@ class TestConfiguratorTracingInit:
         test_configurator._custom_init_tracing(
             exporters={"valid_exporter": SolarWindsSpanExporter},
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_tracerprovider.assert_called_once_with(
+        mocks["mock_tracerprovider"].assert_called_once_with(
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_set_tracer_provider.assert_called_once()
-        mock_tracerprovider_instance.add_span_processor.assert_called_once_with(
+        mocks["mock_set_tracer_provider"].assert_called_once()
+        mocks["mock_tracerprovider_instance"].add_span_processor.assert_called_once_with(
             mock_bsprocessor.return_value,
         )
-        mock_http_exporter.assert_called_once()
-        mock_sw_exporter.assert_not_called()
-        mock_bsprocessor.assert_called_once_with(mock_http_exporter.return_value)
+        mocks["mock_http_exporter"].assert_called_once()
+        mocks["mock_sw_exporter"].assert_not_called()
+        mock_bsprocessor.assert_called_once_with(mocks["mock_http_exporter"].return_value)
         mock_ssprocessor.assert_not_called()
 
     def test_custom_init_tracing_legacy_traces_exporter_protocol_grpc(
@@ -295,38 +259,11 @@ class TestConfiguratorTracingInit:
         mock_bsprocessor,
         mock_ssprocessor,
     ):
-        mocker.patch.dict(
-            os.environ,
-            {
-                "OTEL_EXPORTER_OTLP_PROTOCOL": "grpc",
-            },
-            clear=True
-        )
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarWindsApmConfig",
-            return_value=mock_apmconfig_enabled,
-        )
-
-        # Mock Otel
-        mock_tracerprovider = mocker.Mock()
-        mock_tracerprovider_instance = mocker.Mock()
-        mock_tracerprovider.return_value = mock_tracerprovider_instance
-        mocker.patch(
-            "solarwinds_apm.configurator.SolarwindsTracerProvider",
-            new=mock_tracerprovider,
-        )
-        mock_set_tracer_provider = mocker.patch(
-            "solarwinds_apm.configurator.set_tracer_provider",
-        )
-        mock_apm_sampler = mocker.Mock()
-        mock_resource = mocker.Mock()
-
-        # Mock exporters
-        mock_sw_exporter = mocker.patch(
-            "solarwinds_apm.exporter.SolarWindsSpanExporter",
-        )
-        mock_grpc_exporter = mocker.patch(            
-            "solarwinds_apm.configurator.GrpcSpanExporter",
+        mocks = self.setup_each_test(
+            mocker,
+            mock_apmconfig_enabled,
+            exporters={"valid_exporter": SolarWindsSpanExporter},
+            otlp_protocol="grpc",
         )
 
         # Test!
@@ -334,21 +271,21 @@ class TestConfiguratorTracingInit:
         test_configurator._custom_init_tracing(
             exporters={"valid_exporter": SolarWindsSpanExporter},
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_tracerprovider.assert_called_once_with(
+        mocks["mock_tracerprovider"].assert_called_once_with(
             id_generator=None,
-            sampler=mock_apm_sampler,
-            resource=mock_resource,
+            sampler=mocks["mock_apm_sampler"],
+            resource=mocks["mock_resource"],
         )
-        mock_set_tracer_provider.assert_called_once()
-        mock_tracerprovider_instance.add_span_processor.assert_called_once_with(
+        mocks["mock_set_tracer_provider"].assert_called_once()
+        mocks["mock_tracerprovider_instance"].add_span_processor.assert_called_once_with(
             mock_bsprocessor.return_value,
         )
-        mock_grpc_exporter.assert_called_once()
-        mock_sw_exporter.assert_not_called()
-        mock_bsprocessor.assert_called_once_with(mock_grpc_exporter.return_value)
+        mocks["mock_grpc_exporter"].assert_called_once()
+        mocks["mock_sw_exporter"].assert_not_called()
+        mock_bsprocessor.assert_called_once_with(mocks["mock_grpc_exporter"].return_value)
         mock_ssprocessor.assert_not_called()
 
     def test_custom_init_tracing_legacy_traces_exporter_protocol_unknown(
