2b33f89346acea9dc903c7a9f4c3bb39b5cb9bf9
Jerry Leung <tingfung.leung@solarwinds.com>
2025-04-11 14:31:02 -0700

    nits

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -3,15 +3,10 @@
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-import unittest
-from unittest import mock
-
-import opentelemetry.trace as trace_api
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.sampling import ParentBased
-from opentelemetry.trace import NoOpTracerProvider, set_tracer_provider
+from opentelemetry.trace import NoOpTracerProvider
 
-from solarwinds_apm import configurator
 from solarwinds_apm.api import (
     set_transaction_name,
     solarwinds_ready,
